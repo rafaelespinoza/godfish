0aa9a81916d8c985fa631482548dd38df4ef743d
Rafael Espinoza <rafael@rafaelespinoza.com>
2019-11-30 13:24:24 -0800

    test: Update tests
    Add test for listing versions to apply
    
    It's a helper function, though I'm having trouble with the algorithm.
    It is based off the "comm" command from GNU coreutils, which compares
    sorted lines and selects certain ones. This function will be a central
    part in choosing which migration to run, so it's very important that it
    works properly.

--- godfish_test.go
+++ godfish_test.go
@@ -1,7 +1,6 @@
 package godfish_test
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -22,16 +21,16 @@ func TestMain(m *testing.M) {
 	os.RemoveAll(baseTestOutputDir)
 }
 
-func TestMigration(t *testing.T) {
+func TestMigrationParams(t *testing.T) {
 	var testDir *os.File
-	var mig *godfish.Migration
-	var dbHandler *sql.DB
+	var mig *godfish.MigrationParams
+	var driver godfish.Driver
 	var err error
 	if testDir, err = os.Open(baseTestOutputDir); err != nil {
 		t.Error(err)
 		return
 	}
-	if mig, err = godfish.NewMigration("foo", true, testDir); err != nil {
+	if mig, err = godfish.NewMigrationParams("foo", true, testDir); err != nil {
 		t.Error(err)
 		return
 	}
@@ -47,15 +46,15 @@ func TestMigration(t *testing.T) {
 			godfish.DirReverse, mig.Reverse.Direction(),
 		)
 	}
-	mutations := []godfish.Mutation{mig.Forward, mig.Reverse}
-	for _, mut := range mutations {
-		if mut.Name() != "foo" {
+	migrations := []godfish.Migration{mig.Forward, mig.Reverse}
+	for _, mig := range migrations {
+		if mig.Name() != "foo" {
 			t.Errorf(
 				"wrong Name; expected %s, got %s",
-				"foo", mut.Name(),
+				"foo", mig.Name(),
 			)
 		}
-		if mut.Timestamp().IsZero() {
+		if mig.Timestamp().IsZero() {
 			t.Error("got empty Timestamp")
 		}
 	}
@@ -93,24 +92,31 @@ func TestMigration(t *testing.T) {
 			)
 		}
 	}
-	params := godfish.PGParams{
+
+	driver, err = godfish.NewDriver("postgres", godfish.PGParams{
 		Encoding: "UTF8",
 		Host:     "localhost",
 		Name:     testDBName,
 		Pass:     os.Getenv("DB_PASSWORD"),
 		Port:     "5432",
-	}
-	if dbHandler, err = godfish.Connect("postgres", params); err != nil {
+	})
+	if err != nil {
 		t.Error(err)
 		return
 	}
-	for i, mut := range mutations {
-		pathToFile, err := godfish.PathToMutationFile(baseTestOutputDir, mut)
+	for i, mig := range migrations {
 		if err != nil {
 			t.Errorf("test %d %v\n", i, err)
 			return
 		}
-		if err = godfish.RunMutation(dbHandler, pathToFile); err != nil {
+		timestamp := mig.Timestamp()
+		err = godfish.ApplyMigration(
+			driver,
+			mig.Direction(),
+			baseTestOutputDir,
+			timestamp.Format(godfish.TimeFormat),
+		)
+		if err != nil {
 			t.Errorf("test %d %v\n", i, err)
 			return
 		}
@@ -148,35 +154,35 @@ func TestFilename(t *testing.T) {
 	}
 }
 
-func mustMakeMutation(direction godfish.Direction) godfish.Mutation {
+func mustMakeMigration(direction godfish.Direction) godfish.Migration {
 	ts, err := time.Parse("20060102150405", "20191118121314")
 	if err != nil {
 		panic(err)
 	}
-	mod, err := godfish.NewModification(ts, direction, "test")
+	mut, err := godfish.NewMutation(ts, direction, "test")
 	if err != nil {
 		panic(err)
 	}
-	return mod
+	return mut
 }
 
-func TestMutation(t *testing.T) {
+func TestMigration(t *testing.T) {
 	tests := []struct {
 		filename godfish.Filename
-		expected godfish.Mutation
+		expected godfish.Migration
 	}{
 		{
 			filename: godfish.Filename("20191118121314.forward.test.sql"),
-			expected: mustMakeMutation(godfish.DirForward),
+			expected: mustMakeMigration(godfish.DirForward),
 		},
 		{
 			filename: godfish.Filename("20191118121314.reverse.test.sql"),
-			expected: mustMakeMutation(godfish.DirReverse),
+			expected: mustMakeMigration(godfish.DirReverse),
 		},
 	}
 
 	for i, test := range tests {
-		actual, err := godfish.ParseMutation(test.filename)
+		actual, err := godfish.ParseMigration(test.filename)
 		if err != nil {
 			t.Error(err)
 			return
@@ -198,6 +204,101 @@ func TestMutation(t *testing.T) {
 				"test %d; wrong Timestamp; expected %s, got %s",
 				i, test.expected.Timestamp(), actual.Timestamp(),
 			)
+		}
+	}
+}
+
+func TestListVersionsToApply(t *testing.T) {
+	tests := []struct {
+		direction   godfish.Direction
+		applied     []string
+		available   []string
+		expectedOut []string
+		expectError bool
+	}{
+		{
+			direction:   godfish.DirForward,
+			applied:     []string{"1234", "5678"},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{},
+		},
+		{
+			direction:   godfish.DirForward,
+			applied:     []string{"1234"},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{"5678"},
+		},
+		{
+			direction:   godfish.DirForward,
+			applied:     []string{},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{"1234", "5678"},
+		},
+		{
+			direction:   godfish.DirReverse,
+			applied:     []string{"1234", "5678"},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{"1234", "5678"},
+		},
+		{
+			direction:   godfish.DirReverse,
+			applied:     []string{"1234"},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{"1234"},
+		},
+		{
+			direction:   godfish.DirReverse,
+			applied:     []string{},
+			available:   []string{"1234", "5678"},
+			expectedOut: []string{},
+		},
+		{
+			direction:   godfish.DirForward,
+			applied:     []string{},
+			available:   []string{},
+			expectedOut: []string{},
+		},
+		{
+			direction:   godfish.DirReverse,
+			applied:     []string{},
+			available:   []string{},
+			expectedOut: []string{},
+		},
+		{
+			applied:     []string{},
+			available:   []string{},
+			expectError: true,
+		},
+	}
+
+	for i, test := range tests {
+		actual, err := godfish.ListVersionsToApply(
+			test.direction,
+			test.applied,
+			test.available,
+		)
+		gotError := err != nil
+		if gotError && !test.expectError {
+			t.Errorf("test %d; got error %v but did not expect one", i, err)
+			continue
+		} else if !gotError && test.expectError {
+			t.Errorf("test %d; did not get error but did expect one", i)
+			continue
+		}
+		if len(actual) != len(test.expectedOut) {
+			t.Errorf(
+				"test %d; got wrong output length %d, expected length to be %d",
+				i, len(actual), len(test.expectedOut),
+			)
+			continue
+		}
+		for j, version := range actual {
+			if version != test.expectedOut[j] {
+				t.Errorf(
+					"test [%d][%d]; got version %q but expected %q",
+					i, j, version, test.expectedOut[j],
+				)
+			}
 		}
 	}
 }
