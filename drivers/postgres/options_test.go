812af516817d9076e9ed5fe86429bc234ab4f857
Rafael Espinoza <rafael@rafaelespinoza.com>
2026-08-09 09:34:35 -0700

    (postgres): Add DriverOptions

---
+++ drivers/postgres/options_test.go
@@ -0,0 +1,74 @@
+package postgres
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	"github.com/rafaelespinoza/godfish/internal"
+)
+
+func TestDriverWithDB(t *testing.T) {
+	dsn := os.Getenv(internal.DSNKey)
+	if dsn == "" {
+		t.Fatalf("define env var %q for these tests", internal.DSNKey)
+	}
+
+	t.Run("connection managed outside of library", func(t *testing.T) {
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Cleanup(func() {
+			cerr := db.Close()
+			if cerr != nil {
+				t.Logf("closing db in cleanup: %s", cerr.Error())
+			}
+		})
+
+		driver := NewDriver()
+		if err = driver.WithDriverOptions(WithDB(db)); err != nil {
+			t.Fatal(err)
+		}
+		if driver.connOwned {
+			t.Error("driver not expected to own connection")
+		}
+
+		if err = driver.Close(); err != nil {
+			t.Errorf("unexpected error closing DB: %s", err.Error())
+		}
+		if driver.connection == nil {
+			t.Fatal("driver connection unexpectedly nil")
+		}
+		// Despite calling the driver.Close method, the underlying
+		// connection remains open.
+		if err = driver.connection.PingContext(t.Context()); err != nil {
+			t.Errorf("unexpected Ping error: %s", err.Error())
+		}
+	})
+
+	t.Run("connection managed within library", func(t *testing.T) {
+		driver := NewDriver()
+		err := driver.Connect(dsn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !driver.connOwned {
+			t.Error("driver expected to own connection")
+		}
+
+		t.Cleanup(func() {
+			cerr := driver.Close()
+			if cerr != nil {
+				t.Logf("closing db in cleanup: %s", cerr.Error())
+			}
+		})
+
+		if err = driver.Close(); err != nil {
+			t.Errorf("unexpected error closing DB: %s", err.Error())
+		}
+		if driver.connection != nil {
+			t.Fatal("driver connection unexpectedly non-nil")
+		}
+	})
+}
